37d6255379381a1329baea3637a14ff3a8ff4dc8
bort-andrea <bortolotti.andrea93@gmail.com>
2024-10-24 13:33:44 +0200

    Codecademy ex

---
+++ projects/2. C#/Codecademy/PasswordChecker/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace PasswordChecker
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+        int minLength = 8;
+        string upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        string lowerCase = "abcdefghijklmnopqrstuvwxyz";
+        string digits = "0123456789";
+        string specialChars = "!#?@<>";
+
+        Console.WriteLine("Inserisci una password: ");
+        string password = Console.ReadLine();
+
+        int score = 0;
+
+        if(password.Length >= minLength){
+          score ++;
+        }
+        if(Tools.Contains(password,upperCase)){
+          score ++;
+        }
+        if(Tools.Contains(password,lowerCase)){
+          score ++;
+        }
+        if(Tools.Contains(password,digits)){
+          score++;
+        }
+        if(Tools.Contains(password,specialChars)){
+          score++;
+        }
+        Console.WriteLine(score);
+    }
+  }
+}
